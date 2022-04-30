b7442d1176b1e28d7e4417d9820af3429efbcf1e
@sep0225 <sep0225@live.unc.edu>
2022-04-30 17:51:33 -0400

    another one

--- server.js
+++ server.js
@@ -1,150 +1,89 @@
-// Require Express.js
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
+const fs = require('fs')
+const db = require("./database.js")
 
-const min = require('minimist')
-const args = min(process.argv.slice(2))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-args['port']
-const port = args.port || process.env.PORT || 5000
+const args = require('minimist')(process.argv.slice(2))
 
-// Start an app server
+args['port', 'debug', 'log', 'help']
+const port = args.port || process.env.PORT || 5555;
+
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
 
+if (args.help == true) {
+  const help = (`server.js [options]
+
+  --por		Set the port number for the server to listen on. Must be an integer
+              	between 1 and 65535.
+
+  --debug	If set to true, creates endlpoints /app/log/access/ which returns
+              	a JSON access log from the database and /app/error which throws 
+              	an error with the message "Error test successful." Defaults to 
+		false.
+
+  --log		If set to false, no log files are written. Defaults to true.
+		Logs are always written to database.
+
+  --help	Return this message and exit.`)
+  console.log(help)
+  process.exit(0)
+}
+
+if (args.log == true) {
+  const morgan = require('morgan')
+  const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
+  app.use(morgan('combined', { stream: accessLog }))
+}
+
+app.use((req, res, next) => {
+  let logdata = {remoteaddr: req.ip,
+                remoteuser: req.user,
+                time: Date.now(),
+                method: req.method,
+                url: req.url,
+                protocol: req.protocol,
+                httpversion: reqhttpVersion,
+                statis: res.statusCode,
+                referer: req.headers['referer'],
+                useragent: req.headers['user-agent']
+              }
+  const stmt = db.prepare(`INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`)
+  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url,
+  logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+  next()
+
+})
+
+
+
 app.get('/app/', (req, res) => {
-    // Respond with status 200
     res.statusCode = 200;
-    // Respond with status message "OK"
     res.statusMessage = 'OK';
     res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
     res.end(res.statusCode+ ' ' +res.statusMessage)
 });
 
-app.get('/app/flip/', (req, res) => {
-    let flip = coinFlip()
-    res.statusCode = 200;
-    res.json({ 'flip': flip })
-})
+if (args.debug == true) {
+  app.get('/app/log/access', (req, res) => {
+    try {
+      const stmt = db.prepare('SELECT * FROM accesslog').all()
+      res.status(200).json(stmt)
+    } catch {
+      console.error(e)
+    }
+  });
+  app.get('/app/error', (req, res) => {
+    res.status(500);
+    throw new Error('Error test completed successfully.')
+  })
+}
 
-app.get('/app/flips/:number', (req, res) => {
-    let flips = coinFlips(req.params.number)
-    let total = countFlips(flips)
-    res.statusCode = 200
-    res.json({ 'raw': flips, 'summary': total })
-})
-
-app.get('/app/flip/call/heads', (req, res) => {
-    let heads = flipACoin('heads')
-    res.statusCode = 200
-    res.json(heads)
-})
-
-app.get('/app/flip/call/tails', (req, res) => {
-    let tails = flipACoin('tails')
-    res.statusCode = 200
-    res.json(tails)
-})
-
-// Default response for any other request
 app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
+  res.status(404).send('404 NOT FOUND')
 });
-
-/** Simple coin flip
- * 
- * Write a function that accepts no parameters but returns either heads or tails at random.
- * 
- * @param {*}
- * @returns {string} 
- * 
- * example: coinFlip()
- * returns: heads
- * 
- */
-
- function coinFlip() {
-    return Math.random() < 0.6 ? ("heads") : ("tails")
-  }
-  
-  /** Multiple coin flips
-   * 
-   * Write a function that accepts one parameter (number of flips) and returns an array of 
-   * resulting "heads" or "tails".
-   * 
-   * @param {number} flips 
-   * @returns {string[]} results
-   * 
-   * example: coinFlips(10)
-   * returns:
-   *  [
-        'heads', 'heads',
-        'heads', 'tails',
-        'heads', 'tails',
-        'tails', 'heads',
-        'tails', 'heads'
-      ]
-   */
-  
-  function coinFlips(flips) {
-    const arr = [];
-    for (let i = 0; i < flips; i++) {
-      arr[i] = coinFlip();
-    }
-    return arr;
-  }
-  
-  /** Count multiple flips
-   * 
-   * Write a function that accepts an array consisting of "heads" or "tails" 
-   * (e.g. the results of your `coinFlips()` function) and counts each, returning 
-   * an object containing the number of each.
-   * 
-   * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
-   * { tails: 5, heads: 5 }
-   * 
-   * @param {string[]} array 
-   * @returns {{ heads: number, tails: number }}
-   */
-  
-  function countFlips(array) {
-    let h_amt = 0;
-    let t_amt = 0;
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] == "heads") {
-        h_amt += 1;
-      } else {
-        t_amt += 1;
-      }
-    }
-    if (h_amt == 0) {
-      return "{ tails: " + t_amt + " }";
-    }
-    if (t_amt == 0) {
-      return "{ heads: " + h_amt + " }";
-    }
-    return "{ heads: " + h_amt + ", tails: " + t_amt + " }";
-  }
-  
-  /** Flip a coin!
-   * 
-   * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
-   * 
-   * @param {string} call 
-   * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
-   * 
-   * example: flipACoin('tails')
-   * returns: { call: 'tails', flip: 'heads', result: 'lose' }
-   */
-  
-  function flipACoin(call) {
-    let flip = coinFlip();
-    let result = "";
-    if (call == flip) {
-      result = "win";
-    } else {
-      result = "lose";
-    }
-    return "{ call: '" + call + "', flip: '" + flip + "', result: '" + result + "' }";
-  }

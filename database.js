66b6fa754f28b65a38015b892e7905f72e55ca12
@sep0225 <sep0225@live.unc.edu>
2022-05-01 19:33:42 -0400

    adding

--- database.js
+++ database.js
@@ -4,24 +4,23 @@ const database = require('better-sqlite3')
 
 const db = new database('log.db')
 
-const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name = 'accesslog'`)
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name = 'accesslog';`);
 
 let row = stmt.get()
 
 if (row === undefined) {
-    console.log('Your log database is missing. Creating log database now.')
     const sqlInit = `CREATE TABLE accesslog ( 
                         id INTEGER PRIMARY KEY, 
-                        remoteaddr VARCHAR, 
-                        remoteuser VARCHAR, 
-                        time VARCHAR,
-                        method VARCHAR,
+                        remoteaddr TEXT, 
+                        remoteuser TEXT, 
+                        time INTEGER,
+                        method TEXT,
                         url TEXT,
                         protocol TEXT,
                         httpversion TEXT,
-                        status TEXT,
+                        status INTEGER,
                         referrer TEXT,
-                        useragent TEXT );`
+                        useragent TEXT);`;
     db.exec(sqlInit)
 } else {
     console.log("Log database already exists.")
